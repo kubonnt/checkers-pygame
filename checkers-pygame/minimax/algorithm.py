e863d8db55ac66eb304b1f7a0eb532f9044f8155
kubonnt <kuba.sm.WTF@gmail.com>
2021-03-07 10:59:23 +0100

    first commit

---
+++ checkers-pygame/minimax/algorithm.py
@@ -0,0 +1,67 @@
+from copy import deepcopy
+import pygame
+
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+'''
+The position argument is the current position that we are in, it will be a board object.
+I will pass different boards to the minimax algorithm and based on this board, the algorithm, 
+will give me the best possible board after this that I should move in.
+The depth argument is telling how far we are making this tree.
+The max_polayer is going to be a boolean telling us if we minimize or maximize the value.
+'''
+def minimax(position, depth, max_player, game):
+    if depth == 0 or position.winner() != None:
+        return position.evaluate(), position
+    
+    if max_player:
+        maxEval = float('-inf')
+        best_move = None
+        for move in get_all_moves(position, WHITE, game):
+            evaluation = minimax(move, depth-1, False, game)[0]
+            maxEval = max(maxEval, evaluation)
+            if maxEval == evaluation:
+                best_move = move
+
+        return maxEval, best_move
+    else:
+        minEval = float('inf')
+        best_move = None
+        for move in get_all_moves(position, RED, game):
+            evaluation = minimax(move, depth-1, True, game)[0]
+            minEval = min(minEval, evaluation)
+            if minEval == evaluation:
+                best_move = move
+
+        return minEval, best_move
+
+def simulate_move(piece, move, board, game, skip):
+    board.move(piece, move[0], move[1])
+    if skip:
+        board.remove(skip)
+
+    return board
+
+def get_all_moves(board, color, game):
+    moves = []
+
+    for piece in board.get_all_pieces(color):
+        valid_moves = board.get_valid_moves(piece)
+        for move, skip in valid_moves.items():
+            #draw_moves(game, board, piece)
+            temp_board = deepcopy(board)
+            temp_piece = temp_board.get_piece(piece.row, piece.col)
+            new_board = simulate_move(temp_piece, move, temp_board, game, skip)
+            moves.append(new_board)
+
+    return moves
+
+def draw_moves(game, board, piece):
+    valid_moves = board.get_valid_moves(piece)
+    board.draw(game.win)
+    pygame.draw.circle(game.win, (0, 255, 0), (piece.x, piece.y), 50, 5)
+    game.draw_valid_moves(valid_moves.keys())
+    pygame.display.update()
+    #pygame.time.delay(100)
+
